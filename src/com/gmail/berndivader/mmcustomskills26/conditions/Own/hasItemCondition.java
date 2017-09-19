963df198ef4becbd690ee4bb0bdb24e2fdc0b56b
BuildTools <kasermandel@gmail.com>
2017-09-19 17:17:05 +0200

    added hasitem condition

--- src/com/gmail/berndivader/mmcustomskills26/conditions/Own/hasItemCondition.java
+++ src/com/gmail/berndivader/mmcustomskills26/conditions/Own/hasItemCondition.java
@@ -1,14 +1,13 @@
 package com.gmail.berndivader.mmcustomskills26.conditions.Own;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.LivingEntity;
 
-import com.gmail.berndivader.mmcustomskills26.Main;
 import com.gmail.berndivader.mmcustomskills26.conditions.mmCustomCondition;
 
 import io.lumine.xikage.mythicmobs.adapters.AbstractEntity;
@@ -17,6 +16,8 @@ import io.lumine.xikage.mythicmobs.skills.SkillString;
 import io.lumine.xikage.mythicmobs.skills.conditions.IEntityCondition;
 import io.lumine.xikage.mythicmobs.util.types.RangedDouble;
 import me.lucko.helper.Scheduler;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 public class hasItemCondition extends mmCustomCondition
 implements
@@ -24,7 +25,7 @@ IEntityCondition {
 	
 	private enum WhereType {
 		HAND,
-		EQUIPPED,
+		ARMOR,
 		INVENTORY,
 		ANY;
 		private WhereType() {
@@ -61,7 +62,7 @@ IEntityCondition {
 				this.matAny=true;
 				return;
 			}
-			Material material=null;
+			Material material;
 			try {
 				material = Material.valueOf(m.toUpperCase());
 			} catch (Exception ex) {
@@ -77,15 +78,13 @@ IEntityCondition {
 		public void setAmount(String a) {
 			this.amount=a==null||a.isEmpty()?new RangedDouble("0to64"):new RangedDouble(a);
 		}
-		public void setWhere(String w) {
-			this.where=WhereType.get(w);
-		}
+		public void setWhere(String w) { this.where=WhereType.get(w); }
 		public boolean isMaterialAny(){
 			return this.matAny;
 		}
 	}
 	
-	private final HashSet<ItemHolding> holdinglist;
+	private HashSet<ItemHolding> holdinglist;
 	
 	public hasItemCondition(String line, MythicLineConfig mlc) {
 		super(line, mlc);
@@ -93,16 +92,14 @@ IEntityCondition {
 		final String tmp=mlc.getString(new String[]{"list","l"},null);
 		if (tmp!=null) {
 			String[]list=tmp.split(",");
-			for(int a=0;a<list.length;a++) {
+			Arrays.stream(list).forEach(parse-> {
 				ItemHolding itemholding=new ItemHolding();
-				String parse=list[a];
 				parse=SkillString.parseMessageSpecialChars(parse);
 				if (parse.startsWith("\"")
 						&& parse.endsWith("\"")) {
 					parse=parse.substring(1, parse.length()-1);
 					String[]p=parse.split(";");
-					for (int b=0;b<p.length;b++) {
-						String parse1=p[b];
+					Arrays.stream(p).forEach(parse1->{
 						if(parse1.startsWith("material=")) {
 							parse1=parse1.substring(9, parse1.length());
 							itemholding.setMaterial(parse1);
@@ -116,10 +113,10 @@ IEntityCondition {
 							parse1=parse1.substring(6,parse1.length());
 							itemholding.setWhere(parse1);
 						}
-					}
+					});
 				}
-				if (itemholding!=null) this.holdinglist.add(itemholding);
-			}
+				this.holdinglist.add(itemholding);
+			});
 		}
 	}
 
@@ -127,26 +124,27 @@ IEntityCondition {
 	public boolean check(AbstractEntity t) {
 		boolean check=false;
 		if (t.isLiving()) {
-			final boolean isPlayer=t.isPlayer()?true:false;
+			final boolean isPlayer=t.isPlayer();
 			final LivingEntity target=(LivingEntity)t.getBukkitEntity();
 			final CompletableFuture<Boolean>bool=Scheduler.supplyAsync(()-> {
-				boolean match=false;
 				for(ItemHolding entry:hasItemCondition.this.holdinglist) {
-					Main.logger.info("Material");
-					if (!entry.isMaterialAny()) {
-						Main.logger.info(entry.material.toString());
-					} else {
-						Main.logger.info("any");
-						match=true;
+					if (entry.where.equals(WhereType.ANY)||entry.where.equals(WhereType.HAND)) {
+						if (checkContent(new ItemStack[]{target.getEquipment().getItemInMainHand()},entry)) {
+							return true;
+						}
 					}
-					Main.logger.info("Lores");
-					Main.logger.info(entry.lore);
-					Main.logger.info("Wheres");
-					Main.logger.info(entry.where.toString());
-					Main.logger.info("Amount");
-					Main.logger.info(entry.amount.toString());
+					if (entry.where.equals(WhereType.ANY)||entry.where.equals(WhereType.ARMOR)) {
+						if (checkContent(target.getEquipment().getArmorContents(),entry)) {
+							return true;
+						}
+					}
+					if (isPlayer&&(entry.where.equals(WhereType.ANY)||entry.where.equals(WhereType.INVENTORY))) {
+ 						if (checkContent(((Player)target).getInventory().getContents(),entry)) {
+							return true;
+						}
+					}
 				}
-				return match;
+				return false;
 			});
 			try {
 				check=bool.get();
@@ -154,8 +152,12 @@ IEntityCondition {
 				e.printStackTrace();
 			}
 		}
-		Main.logger.info(Boolean.toString(check));
 		return check;
 	}
 
+	private static boolean checkContent(ItemStack[]i, ItemHolding entry) {
+		return Arrays.stream(i).filter(is -> is != null
+				&& (entry.isMaterialAny() || entry.material.equals(is.getType()))
+				&& entry.amount.equals(is.getAmount())).anyMatch(is -> entry.lore.equals("ANY") || (is.hasItemMeta() && is.getItemMeta().hasLore() && is.getItemMeta().getLore().contains(entry.lore)));
+	}
 }
