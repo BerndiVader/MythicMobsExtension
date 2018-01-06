9f4083f9889cca9e94542f9e5a18926a73f731ef
muhahahahe <muhahahahe@users.noreply.github.com>
2018-01-06 18:34:18 +0100

    cleanup + changes
    switch cases now in alphabetic order
    canged advrandomskill to customrandomskill

--- src/com/gmail/berndivader/mythicmobsext/CustomMechanics.java
+++ src/com/gmail/berndivader/mythicmobsext/CustomMechanics.java
@@ -22,263 +22,260 @@ public class CustomMechanics implements Listener {
 		mech = e.getMechanicName().toLowerCase();
 		
 		switch (mech) {
-		case "damagearmor": {
-			skill = new DamageArmorMechanic(e.getContainer().getConfigLine(), e.getConfig());
-			e.register(skill);
-			break;
-		} case "unequip": {
-			skill = new UnequipMechanic(e.getContainer().getConfigLine(), e.getConfig());
-			e.register(skill);
-			break;
-		} case "grenade": {
-			skill = new GrenadeMechanic(e.getContainer().getConfigLine(), e.getConfig());
-			e.register(skill);
-			break;
-		} case "setrandomlevel": {
-			skill = new RandomLevelMechanic(e.getContainer().getConfigLine(), e.getConfig());
-			e.register(skill);
-			break;
-		} case "steal": {
-			skill = new StealMechanic(e.getContainer().getConfigLine(), e.getConfig());
-			e.register(skill);
-			break;
-		} case "dropstolenitems": {
-			skill = new DropStolenItemsMechanic(e.getContainer().getConfigLine(),e.getConfig());
-			e.register(skill);
-			break;
-		} case "equipskull": {
-			skill = new EquipFixMechanic(e.getContainer().getConfigLine(), e.getConfig());
-			e.register(skill);
-			break;
-		} case "endereffect": {
-			skill = new EnderDragonDeathEffect(e.getContainer().getConfigLine(), e.getConfig());
-			e.register(skill);
-			break;
-		} case "customdamage": {
-			skill = new CustomDamage(e.getContainer().getConfigLine(), e.getConfig());
-			e.register(skill);
-			break;
-		} case "cure":
-		case "removepotion": {
-			skill = new RemovePotionEffectMechanic(e.getContainer().getConfigLine(), e.getConfig());
-			e.register(skill);
-			break;
-		} case "feed": {
-			skill = new FeedMechanic(e.getContainer().getConfigLine(), e.getConfig());
-			e.register(skill);
-			break;
-		} case "oxygen": {
-			skill = new OxygenMechanic(e.getContainer().getConfigLine(), e.getConfig());
-			e.register(skill);
-			break;
-		} case "customsummon": {
-			skill = new CustomSummonMechanic(e.getContainer().getConfigLine(), e.getConfig());
-			e.register(skill);
-			break;
-		} case "stun": {
-			skill = new StunMechanic(e.getContainer().getConfigLine(), e.getConfig());
-			e.register(skill);
-			break;
-		} case "customteleport": {
-			skill = new CustomTeleportMechanic(e.getContainer().getConfigLine(), e.getConfig());
-			e.register(skill);
-			break;
-		} case "swap": {
-			skill = new SwapMechanic(e.getContainer().getConfigLine(), e.getConfig());
-			e.register(skill);
-			break;
-		} case "setthreattarget": {
-			skill = new SetThreatTableTargetMechanic(e.getContainer().getConfigLine(), e.getConfig());
-			e.register(skill);
-			break;
-		} case "clearthreattarget": {
-			skill = new ClearThreatTableMechanic(e.getContainer().getConfigLine(), e.getConfig());
-			e.register(skill);
-			break;
-		} case "customparticleline": {
-			skill = new CustomParticleLineEffect(e.getContainer().getConfigLine(), e.getConfig());
-			e.register(skill);
-			break;
-		} case "customparticles": {
-			skill = new CustomParticleEffect(e.getContainer().getConfigLine(), e.getConfig());
-			e.register(skill);
-			break;
-		} case "dropmythicitem": {
-			skill = new DropMythicItemMechanic(e.getContainer().getConfigLine(), e.getConfig());
-			e.register(skill);
-			break;
-		} case "itemprojectile": {
-			skill = new ItemProjectile(e.getContainer().getConfigLine(), e.getConfig());
-			e.register(skill);
-			break;
-		} case "blockprojectile": {
-			skill = new BlockProjectile(e.getContainer().getConfigLine(), e.getConfig());
-			e.register(skill);
-			break;
-		} case "entityprojectile": {
-			skill = new EntityProjectile(e.getContainer().getConfigLine(), e.getConfig());
-			e.register(skill);
-			break;
-		} case "mythicprojectile": {
-			skill = new MythicProjectile(e.getContainer().getConfigLine(), e.getConfig());
-			e.register(skill);
-			break;
-		} case "mythicorbitalprojectile": {
-			skill = new MythicOrbitalProjectile(e.getContainer().getConfigLine(), e.getConfig());
-			e.register(skill);
-			break;
-		} case "mythiceffectprojectile": {
-			skill = new EffectProjectile(e.getContainer().getConfigLine(), e.getConfig());
-			e.register(skill);
-			break;
-		} case "setmeta": {
-			skill = new SetMetatagMechanic(e.getContainer().getConfigLine(), e.getConfig());
-			e.register(skill);
-			break;
-		} case "delmeta": {
-			skill = new DeleteMetatagMechanic(e.getContainer().getConfigLine(), e.getConfig());
-			e.register(skill);
-			break;
-		} case "castif": {
-			skill = new CastIf(e.getContainer().getConfigLine(), e.getConfig());
-			e.register(skill);
-			break;
-		} case "parsedstance":
-		  case "pstance": {
-			skill = new ParsedStance(e.getContainer().getConfigLine(),e.getConfig());
-			e.register(skill);
-			break;
-		} case "advaipathfinder": {
-			skill = new AdvAIPathFinderSelector(e.getContainer().getConfigLine(),e.getConfig());
-			e.register(skill);
-			break;
-		} case "advrandomskill": {
-			skill = new AdvRandomSkillMechanic(e.getContainer().getConfigLine(),e.getConfig());
-			e.register(skill);
-			break;
-		}case "renameentity": {
-			skill = new RenameEntityMechanic(e.getContainer().getConfigLine(),e.getConfig());
-			e.register(skill);
-			break;
-		}case "parseddisguise": {
-			skill=new ParsedDisguiseMechanic(e.getContainer().getConfigLine(),e.getConfig());
-			e.register(skill);
-			break;
-		}case "setrotation": {
-			skill=new SetRotationMechanic(e.getContainer().getConfigLine(),e.getConfig());
-			e.register(skill);
-			break;
-		}case "setfaction": {
-			skill=new SetFactionMechanic(e.getContainer().getConfigLine(),e.getConfig());
-			e.register(skill);
-			break;
-		}case "customvelocity": {
-			skill=new CustomVelocityMechanic(e.getContainer().getConfigLine(),e.getConfig());
-			e.register(skill);
-			break;
-		}case "asequip": {
-			skill=new EquipArmorstandMechanic(e.getContainer().getConfigLine(),e.getConfig());
-			e.register(skill);
-			break;
-		}case "playerweather": {
-			skill=new PlayerWeatherMechanic(e.getContainer().getConfigLine(), e.getConfig());
-			e.register(skill);
-			break;
-		}case "itemfloating": {
-			skill=new IStatueMechanic(e.getContainer().getConfigLine(),e.getConfig());
-			e.register(skill);
-			break;
-		}case "blockfloating":{
-			skill=new BStatueMechanic(e.getContainer().getConfigLine(),e.getConfig());
-			e.register(skill);
-			break;
-		}case "entityfloating":{
-			skill=new EStatueMechanic(e.getContainer().getConfigLine(),e.getConfig());
-			e.register(skill);
-			break;
-		}case "mythicfloating":{
-			skill=new MStatueMechanic(e.getContainer().getConfigLine(),e.getConfig());
-			e.register(skill);
-			break;
-		}case "astar":{
-			skill=new AStarMechanic(e.getContainer().getConfigLine(),e.getConfig());
-			e.register(skill);
-			break;
-		}case "storecooldown":{
-			skill=new StoreCooldownMechanic(e.getContainer().getConfigLine(),e.getConfig());
-			e.register(skill);
-			break;
-		}case "extinguish":{
-			skill=new ExtinguishMechanic(e.getContainer().getConfigLine(),e.getConfig());
-			e.register(skill);
-			break;
-		}case "playerspin":{
-			skill=new PlayerSpinMechanic(e.getContainer().getConfigLine(),e.getConfig());
-			e.register(skill);
-			break;
-		}case "playergoggleat":
-		case "playergoggle": {
-			skill=new PlayerGoggleMechanic(e.getContainer().getConfigLine(),e.getConfig());
-			e.register(skill);
-			break;
-		}
-		case "entitygoggle":
-		case "entitylookin":
-		case "entitygoggleat": {
-			skill=new EntityGoogleMechanic(e.getContainer().getConfigLine(),e.getConfig());
-			e.register(skill);
-			break;
-		}case "disarm": {
-			skill=new DisarmPlayerMechanic(e.getContainer().getConfigLine(),e.getConfig());
-			e.register(skill);
-			break;
-		}case "setmobhealth": {
-			skill=new SetHealthMechanic(e.getContainer().getConfigLine(),e.getConfig());
-			e.register(skill);
-			break;
-		}case "playcredits": {
-			skill=new PlayCreditsMechanic(e.getContainer().getConfigLine(),e.getConfig());
-			e.register(skill);
-			break;
-		}case "playloading": {
-			skill=new PlayLoadingMechanic(e.getContainer().getConfigLine(),e.getConfig());
-			e.register(skill);
-			break;
-		}case "fakedeath": {
-			skill=new FakePlayerDeathMechanic(e.getContainer().getConfigLine(),e.getConfig());
-			e.register(skill);
-			break;
-		}case "closeinventory": {
-			skill=new CloseInventoryMechanic(e.getContainer().getConfigLine(),e.getConfig());
-			e.register(skill);
-			break;
-		}case "forcespectate": {
-			skill=new ForceSpectateMechanic(e.getContainer().getConfigLine(),e.getConfig());
-			e.register(skill);
-			break;
-		}case "dropcombat": {
-			skill=new DropCombatMechanic(e.getContainer().getConfigLine(),e.getConfig());
-			e.register(skill);
-			break;
-		}case "dropinventory": {
-			skill=new DropInventoryMechanic(e.getContainer().getConfigLine(),e.getConfig());
-			e.register(skill);
-			break;
-		}case "modifyarrows": {
-			skill=new RemoveArrowsMechanic(e.getContainer().getConfigLine(),e.getConfig());
-			e.register(skill);
-			break;
-		}
-		case "setitemcooldown": {
-			skill=new SetItemCooldown(e.getContainer().getConfigLine(),e.getConfig());
-			e.register(skill);
-			break;
-		}
-		case "playerzoom": {
-			skill=new ZoomPlayerMechanic(e.getContainer().getConfigLine(),e.getConfig());
-			e.register(skill);
-			break;
-		}
+			case "advaipathfinder": {
+				skill = new AdvAIPathFinderSelector(e.getContainer().getConfigLine(),e.getConfig());
+				e.register(skill);
+				break;
+			} case "asequip": {
+				skill=new EquipArmorstandMechanic(e.getContainer().getConfigLine(),e.getConfig());
+				e.register(skill);
+				break;
+			} case "astar": {
+				skill=new AStarMechanic(e.getContainer().getConfigLine(),e.getConfig());
+				e.register(skill);
+				break;
+			} case "blockfloating": {
+				skill=new BStatueMechanic(e.getContainer().getConfigLine(),e.getConfig());
+				e.register(skill);
+				break;
+			} case "blockprojectile": {
+				skill = new BlockProjectile(e.getContainer().getConfigLine(), e.getConfig());
+				e.register(skill);
+				break;
+			} case "castif": {
+				skill = new CastIf(e.getContainer().getConfigLine(), e.getConfig());
+				e.register(skill);
+				break;
+			} case "clearthreattarget": {
+				skill = new ClearThreatTableMechanic(e.getContainer().getConfigLine(), e.getConfig());
+				e.register(skill);
+				break;
+			} case "closeinventory": {
+				skill=new CloseInventoryMechanic(e.getContainer().getConfigLine(),e.getConfig());
+				e.register(skill);
+				break;
+			} case "cure":
+			case "removepotion": {
+				skill = new RemovePotionEffectMechanic(e.getContainer().getConfigLine(), e.getConfig());
+				e.register(skill);
+				break;
+			} case "customdamage": {
+				skill = new CustomDamage(e.getContainer().getConfigLine(), e.getConfig());
+				e.register(skill);
+				break;
+			} case "customparticleline": {
+				skill = new CustomParticleLineEffect(e.getContainer().getConfigLine(), e.getConfig());
+				e.register(skill);
+				break;
+			} case "customparticles": {
+				skill = new CustomParticleEffect(e.getContainer().getConfigLine(), e.getConfig());
+				e.register(skill);
+				break;
+			} case "customrandomskill": {
+				skill = new AdvRandomSkillMechanic(e.getContainer().getConfigLine(),e.getConfig());
+				e.register(skill);
+				break;
+			} case "customsummon": {
+				skill = new CustomSummonMechanic(e.getContainer().getConfigLine(), e.getConfig());
+				e.register(skill);
+				break;
+			} case "customteleport": {
+				skill = new CustomTeleportMechanic(e.getContainer().getConfigLine(), e.getConfig());
+				e.register(skill);
+				break;
+			} case "customvelocity": {
+				skill=new CustomVelocityMechanic(e.getContainer().getConfigLine(),e.getConfig());
+				e.register(skill);
+				break;
+			} case "damagearmor": {
+				skill = new DamageArmorMechanic(e.getContainer().getConfigLine(), e.getConfig());
+				e.register(skill);
+				break;
+			} case "delmeta": {
+				skill = new DeleteMetatagMechanic(e.getContainer().getConfigLine(), e.getConfig());
+				e.register(skill);
+				break;
+			} case "disarm": {
+				skill=new DisarmPlayerMechanic(e.getContainer().getConfigLine(),e.getConfig());
+				e.register(skill);
+				break;
+			} case "dropcombat": {
+				skill=new DropCombatMechanic(e.getContainer().getConfigLine(),e.getConfig());
+				e.register(skill);
+				break;
+			} case "dropinventory": {
+				skill=new DropInventoryMechanic(e.getContainer().getConfigLine(),e.getConfig());
+				e.register(skill);
+				break;
+			} case "dropmythicitem": {
+				skill = new DropMythicItemMechanic(e.getContainer().getConfigLine(), e.getConfig());
+				e.register(skill);
+				break;
+			} case "dropstolenitems": {
+				skill = new DropStolenItemsMechanic(e.getContainer().getConfigLine(),e.getConfig());
+				e.register(skill);
+				break;
+			} case "endereffect": {
+				skill = new EnderDragonDeathEffect(e.getContainer().getConfigLine(), e.getConfig());
+				e.register(skill);
+				break;
+			} case "entityfloating": {
+				skill=new EStatueMechanic(e.getContainer().getConfigLine(),e.getConfig());
+				e.register(skill);
+				break;
+			} case "entitygoggle":
+			case "entitygoggleat":
+			case "entitylookin": {
+				skill=new EntityGoogleMechanic(e.getContainer().getConfigLine(),e.getConfig());
+				e.register(skill);
+				break;
+			} case "entityprojectile": {
+				skill = new EntityProjectile(e.getContainer().getConfigLine(), e.getConfig());
+				e.register(skill);
+				break;
+			} case "equipskull": {
+				skill = new EquipFixMechanic(e.getContainer().getConfigLine(), e.getConfig());
+				e.register(skill);
+				break;
+			} case "extinguish": {
+				skill=new ExtinguishMechanic(e.getContainer().getConfigLine(),e.getConfig());
+				e.register(skill);
+				break;
+			} case "fakedeath": {
+				skill=new FakePlayerDeathMechanic(e.getContainer().getConfigLine(),e.getConfig());
+				e.register(skill);
+				break;
+			} case "feed": {
+				skill = new FeedMechanic(e.getContainer().getConfigLine(), e.getConfig());
+				e.register(skill);
+				break;
+			} case "forcespectate": {
+				skill=new ForceSpectateMechanic(e.getContainer().getConfigLine(),e.getConfig());
+				e.register(skill);
+				break;
+			} case "grenade": {
+				skill = new GrenadeMechanic(e.getContainer().getConfigLine(), e.getConfig());
+				e.register(skill);
+				break;
+			} case "itemfloating": {
+				skill=new IStatueMechanic(e.getContainer().getConfigLine(),e.getConfig());
+				e.register(skill);
+				break;
+			} case "itemprojectile": {
+				skill = new ItemProjectile(e.getContainer().getConfigLine(), e.getConfig());
+				e.register(skill);
+				break;
+			} case "modifyarrows": {
+				skill=new RemoveArrowsMechanic(e.getContainer().getConfigLine(),e.getConfig());
+				e.register(skill);
+				break;
+			} case "mythiceffectprojectile": {
+				skill = new EffectProjectile(e.getContainer().getConfigLine(), e.getConfig());
+				e.register(skill);
+				break;
+			} case "mythicfloating": {
+				skill=new MStatueMechanic(e.getContainer().getConfigLine(),e.getConfig());
+				e.register(skill);
+				break;
+			} case "mythicorbitalprojectile": {
+				skill = new MythicOrbitalProjectile(e.getContainer().getConfigLine(), e.getConfig());
+				e.register(skill);
+				break;
+			} case "mythicprojectile": {
+				skill = new MythicProjectile(e.getContainer().getConfigLine(), e.getConfig());
+				e.register(skill);
+				break;
+			} case "oxygen": {
+				skill = new OxygenMechanic(e.getContainer().getConfigLine(), e.getConfig());
+				e.register(skill);
+				break;
+			} case "parseddisguise": {
+				skill=new ParsedDisguiseMechanic(e.getContainer().getConfigLine(),e.getConfig());
+				e.register(skill);
+				break;
+			} case "parsedstance":
+		  	case "pstance": {
+				skill = new ParsedStance(e.getContainer().getConfigLine(),e.getConfig());
+				e.register(skill);
+				break;
+			} case "playcredits": {
+				skill=new PlayCreditsMechanic(e.getContainer().getConfigLine(),e.getConfig());
+				e.register(skill);
+				break;
+			} case "playergoggle":
+			case "playergoggleat": {
+				skill=new PlayerGoggleMechanic(e.getContainer().getConfigLine(),e.getConfig());
+				e.register(skill);
+				break;
+			} case "playerspin": {
+				skill=new PlayerSpinMechanic(e.getContainer().getConfigLine(),e.getConfig());
+				e.register(skill);
+				break;
+			} case "playerweather": {
+				skill=new PlayerWeatherMechanic(e.getContainer().getConfigLine(), e.getConfig());
+				e.register(skill);
+				break;
+			} case "playerzoom": {
+				skill=new ZoomPlayerMechanic(e.getContainer().getConfigLine(),e.getConfig());
+				e.register(skill);
+				break;
+			} case "playloading": {
+				skill=new PlayLoadingMechanic(e.getContainer().getConfigLine(),e.getConfig());
+				e.register(skill);
+				break;
+			} case "renameentity": {
+				skill = new RenameEntityMechanic(e.getContainer().getConfigLine(),e.getConfig());
+				e.register(skill);
+				break;
+			} case "setfaction": {
+				skill=new SetFactionMechanic(e.getContainer().getConfigLine(),e.getConfig());
+				e.register(skill);
+				break;
+			} case "setitemcooldown": {
+				skill=new SetItemCooldown(e.getContainer().getConfigLine(),e.getConfig());
+				e.register(skill);
+				break;
+			} case "setmeta": {
+				skill = new SetMetatagMechanic(e.getContainer().getConfigLine(), e.getConfig());
+				e.register(skill);
+				break;
+			} case "setmobhealth": {
+				skill=new SetHealthMechanic(e.getContainer().getConfigLine(),e.getConfig());
+				e.register(skill);
+				break;
+			} case "setrandomlevel": {
+				skill = new RandomLevelMechanic(e.getContainer().getConfigLine(), e.getConfig());
+				e.register(skill);
+				break;
+			} case "setrotation": {
+				skill=new SetRotationMechanic(e.getContainer().getConfigLine(),e.getConfig());
+				e.register(skill);
+				break;
+			} case "setthreattarget": {
+				skill = new SetThreatTableTargetMechanic(e.getContainer().getConfigLine(), e.getConfig());
+				e.register(skill);
+				break;
+			} case "steal": {
+				skill = new StealMechanic(e.getContainer().getConfigLine(), e.getConfig());
+				e.register(skill);
+				break;
+			} case "storecooldown": {
+				skill=new StoreCooldownMechanic(e.getContainer().getConfigLine(),e.getConfig());
+				e.register(skill);
+				break;
+			} case "stun": {
+				skill = new StunMechanic(e.getContainer().getConfigLine(), e.getConfig());
+				e.register(skill);
+				break;
+			} case "swap": {
+				skill = new SwapMechanic(e.getContainer().getConfigLine(), e.getConfig());
+				e.register(skill);
+				break;
+			} case "unequip": {
+				skill = new UnequipMechanic(e.getContainer().getConfigLine(), e.getConfig());
+				e.register(skill);
+				break;
+			}
 		}
 	}
 }
