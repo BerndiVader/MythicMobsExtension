d35db2bfdba43a8bf302142bb11d34677455ebf7
muhahahahe <muhahahahe@users.noreply.github.com>
2018-01-10 18:48:45 +0100

    cleanup

--- src/com/gmail/berndivader/mythicmobsext/SetFactionMechanic.java
+++ src/com/gmail/berndivader/mythicmobsext/SetFactionMechanic.java
@@ -6,30 +6,32 @@ import io.lumine.xikage.mythicmobs.mobs.ActiveMob;
 import io.lumine.xikage.mythicmobs.skills.*;
 
 public class SetFactionMechanic
-        extends
-        SkillMechanic
+extends
+SkillMechanic
 implements
 ITargetedEntitySkill,
 INoTargetSkill {
-    protected String faction;
-    public SetFactionMechanic(String line, MythicLineConfig mlc) {
-        super(line, mlc);
-        String f=mlc.getString("faction",null);
-        if (f!=null) {
-            this.faction=SkillString.unparseMessageSpecialChars(f);
-        }
-    }
+	
+	protected String faction;
+	public SetFactionMechanic(String line, MythicLineConfig mlc) {
+		
+		super(line, mlc);
+		String f=mlc.getString(new String[] { "faction", "f" }, null);
+		if (f!=null) {
+			this.faction=SkillString.unparseMessageSpecialChars(f);
+		}
+	}
 
-    @Override
-    public boolean cast(SkillMetadata data) {
-        return this.castAtEntity(data, data.getCaster().getEntity());
-    }
+	@Override
+	public boolean cast(SkillMetadata data) {
+		return this.castAtEntity(data, data.getCaster().getEntity());
+	}
 
-    @Override
-    public boolean castAtEntity(SkillMetadata data, AbstractEntity target) {
-        ActiveMob am = Main.mythicmobs.getMobManager().getMythicMobInstance(target);
-        String f=SkillString.parseMobVariables(this.faction,data.getCaster(),target,data.getTrigger());
-        if (am!=null) am.setFaction(f);
-        return true;
-    }
+	@Override
+	public boolean castAtEntity(SkillMetadata data, AbstractEntity target) {
+		ActiveMob am = Main.mythicmobs.getMobManager().getMythicMobInstance(target);
+		String f=SkillString.parseMobVariables(this.faction,data.getCaster(),target,data.getTrigger());
+		if (am!=null) am.setFaction(f);
+		return true;
+	}
 }
