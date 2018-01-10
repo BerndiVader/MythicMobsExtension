75dd1a480ddcdf4163e3842ba43c6fde05ff6ba5
muhahahahe <muhahahahe@users.noreply.github.com>
2018-01-10 18:00:02 +0100

    cleanup

--- src/com/gmail/berndivader/mythicmobsext/RenameEntityMechanic.java
+++ src/com/gmail/berndivader/mythicmobsext/RenameEntityMechanic.java
@@ -6,25 +6,27 @@ import io.lumine.xikage.mythicmobs.skills.*;
 import org.bukkit.entity.LivingEntity;
 
 public class RenameEntityMechanic extends SkillMechanic
-        implements
-        ITargetedEntitySkill {
-    private String name;
-    private boolean v;
-    public RenameEntityMechanic(String line, MythicLineConfig mlc) {
-        super(line,mlc);
-        this.name=mlc.getString(new String[]{"name","n"},"");
-        this.v=mlc.getBoolean("visible",false);
-    }
+implements
+ITargetedEntitySkill {
+	
+	private String name;
+	private Boolean v;
+	public RenameEntityMechanic(String line, MythicLineConfig mlc) {
 
-    @Override
-    public boolean castAtEntity(SkillMetadata data, AbstractEntity target) {
-        if (target.isLiving()&&!target.isPlayer()&&this.name!=null) {
-            String n= SkillString.parseMobVariables(this.name,data.getCaster(),target,data.getTrigger());
-            LivingEntity e = (LivingEntity)target.getBukkitEntity();
-            e.setCustomName(n);
-            e.setCustomNameVisible(this.v);
-            return true;
-        }
-        return false;
-    }
+		super(line,mlc);
+		this.name=mlc.getString(new String[] { "name", "n" }, "");
+		this.v=mlc.getBoolean(new String[] { "visible", "v" }, false);
+	}
+
+	@Override
+	public boolean castAtEntity(SkillMetadata data, AbstractEntity target) {
+		if (target.isLiving()&&!target.isPlayer()&&this.name!=null) {
+			String n= SkillString.parseMobVariables(this.name,data.getCaster(),target,data.getTrigger());
+			LivingEntity e = (LivingEntity)target.getBukkitEntity();
+			e.setCustomName(n);
+			e.setCustomNameVisible(this.v);
+			return true;
+		}
+		return false;
+	}
 }
