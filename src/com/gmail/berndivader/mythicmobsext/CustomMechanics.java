469f57235638d41586b3fc30507de98eb45a3029
muhahahahe <muhahahahe@users.noreply.github.com>
2018-01-12 23:55:53 +0100

    changed SetHealthMechanic to SetMobHealthMechanic

--- src/com/gmail/berndivader/mythicmobsext/CustomMechanics.java
+++ src/com/gmail/berndivader/mythicmobsext/CustomMechanics.java
@@ -240,7 +240,7 @@ public class CustomMechanics implements Listener {
 				e.register(skill);
 				break;
 			} case "setmobhealth": {
-				skill=new SetHealthMechanic(e.getContainer().getConfigLine(),e.getConfig());
+				skill=new SetMobHealthMechanic(e.getContainer().getConfigLine(),e.getConfig());
 				e.register(skill);
 				break;
 			} case "setrandomlevel": {
