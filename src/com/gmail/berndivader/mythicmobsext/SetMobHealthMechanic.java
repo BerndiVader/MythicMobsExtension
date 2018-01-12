fc5666ad52fb656ef54bf7b4ffb0e3867e03451a
muhahahahe <muhahahahe@users.noreply.github.com>
2018-01-12 23:29:06 +0100

    cleanup + changed mode values

--- src/com/gmail/berndivader/mythicmobsext/SetMobHealthMechanic.java
+++ src/com/gmail/berndivader/mythicmobsext/SetMobHealthMechanic.java
@@ -11,19 +11,20 @@ import io.lumine.xikage.mythicmobs.skills.INoTargetSkill;
 import io.lumine.xikage.mythicmobs.skills.SkillMechanic;
 import io.lumine.xikage.mythicmobs.skills.SkillMetadata;
 
-public class SetHealthMechanic 
+public class SetMobHealthMechanic 
 extends
 SkillMechanic 
 implements
 INoTargetSkill {
-	private String r,m;
-	private boolean b;
+	private String r;
+	private String m;
+	private Boolean b;
 
-	public SetHealthMechanic(String skill, MythicLineConfig mlc) {
+	public SetMobHealthMechanic(String skill, MythicLineConfig mlc) {
 		super(skill, mlc);
-		this.r=mlc.getString(new String[] {"amount","a","health","h"},"20");
-		this.b=mlc.getBoolean(new String[] {"ihm","ignoremodifier"},true);
-		this.m=mlc.getString(new String[] {"set"},"");
+		this.r=mlc.getString(new String[] { "amount", "a", "health", "h" }, "20");
+		this.b=mlc.getBoolean(new String[] { "ignoremodifier", "im" }, true);
+		this.m=mlc.getString(new String[] { "mode", "m" }, "").toUpperCase();
 	}
 
 	@Override
@@ -37,17 +38,17 @@ INoTargetSkill {
 				h=Double.parseDouble(this.r);
 			}
 			if (!b&&(data.getCaster() instanceof ActiveMob)) {
-                mod=ConfigManager.defaultLevelModifierHealth.startsWith("+")
+				mod=ConfigManager.defaultLevelModifierHealth.startsWith("ADD")
                 		? Double.valueOf(ConfigManager.defaultLevelModifierHealth.substring(1))
-                		: (ConfigManager.defaultLevelModifierHealth.startsWith("*") 
+                		: (ConfigManager.defaultLevelModifierHealth.startsWith("MULTIPLY") 
                 		? h * Double.valueOf(ConfigManager.defaultLevelModifierHealth.substring(1)) 
                 		: h * Double.valueOf(ConfigManager.defaultLevelModifierHealth));
             }
 			if (am!=null&&am.getLevel()>1&&mod>0.0) h+=mod*(am.getLevel()-1);
 			LivingEntity e=(LivingEntity)data.getCaster().getEntity().getBukkitEntity();
-			if (this.m.equals("+")) {
+			if (this.m.equals("ADD")) {
 				h+=e.getMaxHealth();
-			} else if (this.m.equals("*")) {
+			} else if (this.m.equals("MULTIPLY")) {
 				h=e.getMaxHealth()*h;
 			}
 			h=Math.ceil(h);
