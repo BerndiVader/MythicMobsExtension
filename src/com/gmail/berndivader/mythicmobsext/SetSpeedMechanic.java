e9fefda1092d77d97b629627aefcc5560356a7ad
muhahahahe <muhahahahe@users.noreply.github.com>
2018-01-13 05:17:34 +0100

    cleanup + made it also possible for single values

--- src/com/gmail/berndivader/mythicmobsext/SetSpeedMechanic.java
+++ src/com/gmail/berndivader/mythicmobsext/SetSpeedMechanic.java
@@ -13,42 +13,45 @@ import io.lumine.xikage.mythicmobs.skills.ITargetedEntitySkill;
 import io.lumine.xikage.mythicmobs.skills.SkillMechanic;
 import io.lumine.xikage.mythicmobs.skills.SkillMetadata;
 
-public class RandomSpeedMechanic 
+public class SetSpeedMechanic 
 extends 
 SkillMechanic
 implements
 ITargetedEntitySkill,
 INoTargetSkill {
 	private Range<Double> r1;
-	public static String ss1="0.3to0.45";
 
-	public RandomSpeedMechanic(String skill, MythicLineConfig mlc) {
+	public SetSpeedMechanic(String skill, MythicLineConfig mlc) {
 		super(skill, mlc);
-		String s1=mlc.getString("range",ss1).toUpperCase();
-		if (!s1.contains("TO")) s1=ss1;
-		String[]arr1=s1.split("TO");
-		r1=Range.between(Double.parseDouble(arr1[0]),Double.parseDouble(arr1[1]));
+		String s1=mlc.getString(new String[] { "amount", "a" }, 0.2D).toUpperCase();
+		if (!s1.contains("TO")) {
+			Double d1=s1;
+			r1=null;
+		} else {
+			String[]arr1=s1.split("TO");
+			r1=Range.between(Double.parseDouble(arr1[0]),Double.parseDouble(arr1[1]));
+		}
 	}
 
 	@Override
-	public boolean cast(SkillMetadata data) {
+	public Boolean cast(SkillMetadata data) {
 		return d(data.getCaster().getEntity());
 	}
 
 	@Override
-	public boolean castAtEntity(SkillMetadata data, AbstractEntity target) {
+	public Boolean castAtEntity(SkillMetadata data, AbstractEntity target) {
 		return d(target);
 	}
 	
-	private boolean d(AbstractEntity target) {
+	private Boolean d(AbstractEntity target) {
 		if (target.isLiving()) {
 			LivingEntity le1=(LivingEntity)target.getBukkitEntity();
-			double d1=ThreadLocalRandom.current().nextDouble(r1.getMinimum(),r1.getMaximum());
-            le1.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).setBaseValue(d1);
-    		return true;
+			if (!r1=null) {
+				Double d1=ThreadLocalRandom.current().nextDouble(r1.getMinimum(),r1.getMaximum());
+			}
+			le1.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).setBaseValue(d1);
+			return true;
 		}
 		return false;
 	}
-	
-
 }
