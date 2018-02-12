c821a2cb6d879d878fc7b7af8d75c0621c2bb984
BuildTools <kasermandel@gmail.com>
2018-02-12 22:07:38 +0100

    added new resource files & examples

--- src/main/resources/Scripts.js
+++ src/main/resources/Scripts.js
@@ -1,40 +1,13 @@
-var Bukkit=org.bukkit.Bukkit;
-var mythicmobs=Bukkit.getPluginManager().getPlugin("MythicMobs");
+/*
+Internal Scripts file. Will be rewritten all the time. Better place your 
+scripts into Includes.js or use load in Includes.js to load them.
+*/
 
-var SomeStanceMath=function(data,target,mlc) {
-	if (data.getCaster().getClass().getSimpleName()=='ActiveMob') {
-		var am=data.getCaster();
-		var stance=Number(am.getStance());
-		var value=mlc.getString("math","stance");
-		am.setStance(String(eval(value)));
-	}
-	return true;
-};
 
-var ScoreboardExample=function(data,target,mlc) {
-	if (target instanceof org.bukkit.entity.Entity) {
-		var value=mlc.getString("value");
-		var scoreboard=Bukkit.getServer().getScoreboardManager().getMainScoreboard();
-		var objective=scoreboard.getObjective(mlc.getString("objective"));
-		if (objective!=null) {
-			var sc=objective.getScore(target.getUniqueId().toString());
-			var score=sc.getScore();
-			sc.setScore(eval(value));
-		}
-	}
-	return true;
-}
+load("nashorn:mozilla_compat.js");
+var Bukkit = org.bukkit.Bukkit;
+var mythicmobs = Bukkit.getPluginManager().getPlugin("MythicMobs");
+var mythicmobsext = Bukkit.getPluginManager().getPlugin("MythicMobsExtension");
+var path=mythicmobsext.getDataFolder()+java.io.File.separator;
 
-var BroadcastMythicMobsVersion=function() {
-	Bukkit.getServer().broadcastMessage(mythicmobs.getVersion());
-	return true;
-}
-
-var ChangeTargetsName=function(data,target,mlc) {
-	if (target instanceof org.bukkit.entity.LivingEntity) {
-		target.setCustomName(mlc.getString("newname","whatever"));
-		target.setCustomNameVisible(true);
-		return true;
-	}
-	return false;	
-}
+load(path+"Includes.js");
