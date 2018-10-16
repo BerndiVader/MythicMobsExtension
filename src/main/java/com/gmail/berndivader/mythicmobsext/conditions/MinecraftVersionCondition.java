df9975cef5caca1acbd0de419a609a143b77b11a
BerndiVader <kasermandel@gmail.com>
2018-10-16 09:06:27 +0200

    added minecraftversion condition

---
+++ src/main/java/com/gmail/berndivader/mythicmobsext/conditions/MinecraftVersionCondition.java
@@ -0,0 +1,30 @@
+package com.gmail.berndivader.mythicmobsext.conditions;
+
+import com.gmail.berndivader.mythicmobsext.externals.*;
+import com.gmail.berndivader.mythicmobsext.utils.RangedDouble;
+import com.gmail.berndivader.mythicmobsext.utils.Utils;
+
+import io.lumine.xikage.mythicmobs.adapters.AbstractLocation;
+import io.lumine.xikage.mythicmobs.io.MythicLineConfig;
+import io.lumine.xikage.mythicmobs.skills.conditions.ILocationCondition;
+
+@ExternalAnnotation(name="minecraftversion",author="BerndiVader")
+public class MinecraftVersionCondition 
+extends 
+AbstractCustomCondition 
+implements 
+ILocationCondition {
+	
+	boolean match;
+	
+	public MinecraftVersionCondition(String line, MythicLineConfig mlc) {
+		super(line, mlc);
+		
+		match=(new RangedDouble(mlc.getString("version",">10"))).equals(Utils.serverV);
+	}
+
+	@Override
+	public boolean check(AbstractLocation var1) {
+		return match;
+	}
+}
