1b4f818b6176587154a3e51ae6b92768dec04734
Seyarada <seyarada@gmail.com>
2020-09-20 01:45:56 +0200

    Cleanup of previous commit

--- src/main/java/com/gmail/berndivader/mythicmobsext/mechanics/TransmuteItem.java
+++ src/main/java/com/gmail/berndivader/mythicmobsext/mechanics/TransmuteItem.java
@@ -61,24 +61,18 @@ public class TransmuteItem extends SkillMechanic implements ITargetedEntitySkill
 	public boolean castAtEntity(SkillMetadata data, AbstractEntity p) {
 		
 		int altAmount = Integer.valueOf(amount);
-		Player player = (Player) p.getBukkitEntity();
 		if (p.isPlayer()) {
+			Player player = (Player) p.getBukkitEntity();
 			
 			ItemStack[] baseContent = player.getInventory().getContents();
-			for(int i = 0; i < player.getInventory().getSize(); i++) {
+			for (int i = 0; i < player.getInventory().getSize(); i++) {
 				if(baseContent[i] != null && baseContent[i].isSimilar(baseItem)) {
 		
 					int quantity = baseContent[i].getAmount();
-					if(altAmount<0) {
-						System.out.println("Infinite...");
-						resultItem = new ItemStack(generateItemStack(quantity));
+					if(altAmount < 0) {
+						resultItem = generateItemStack(quantity);
 						player.getInventory().setItem(i, resultItem);
-					}
-					// stack 50
-					// amount 33
-					// stack = 50-33
-					// -> 27
-					
+					}		
 					
 					// Stack is larger than amount
 					else if(quantity > altAmount && altAmount != 0) {
@@ -89,39 +83,15 @@ public class TransmuteItem extends SkillMechanic implements ITargetedEntitySkill
 					
 					// Stack is smaller than amount
 					else if (altAmount != 0) {
-						resultItem = new ItemStack(generateItemStack(quantity));
+						resultItem = generateItemStack(quantity);
 						player.getInventory().setItem(i, resultItem);
 						altAmount = altAmount - quantity;
-					} else {
-						System.out.println("None?");
 					}
 				}
 			}
 		}
 		
-		return false;
+		return true;
 	}
 
 }
-
-/*
-var TransmuteItem=function(data,target,mlc) {
-var baseContent = target.getInventory();
-var invContent = baseContent.getContents();
-var transmuteAmount = mlc.getString("amount");
-//var baseMaterial = mythicmobs.getItemManager().getItemStack(mlc.getString("base"));
-var baseMaterial = mythicmobs.getItemManager().getItem(mlc.getString("base"));
-baseMaterial = baseMaterial.get();
-var outputMaterial = baseMaterial.generateItemStack(1);
-Bukkit.getServer().broadcastMessage(outputMaterial);
-var transmuteMaterial = mythicmobs.getItemManager().getItemStack(mlc.getString("result"));
-transmuteMaterial.setAmount(transmuteAmount);
-for(i = 0; i < invContent.length; i++) {
-    if(invContent[i] !== null && invContent[i].isSimilar(baseMaterial)) {
-        var matchAmount = invContent[i].getAmount();
-        if(matchAmount - transmuteAmount >= 0) {
-            invContent[i].setAmount(matchAmount - transmuteAmount);
-            baseContent.setItem(i, invContent[i]);
-            baseContent.addItem(transmuteMaterial);
-            return true;
-*/
